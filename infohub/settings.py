a43a0ac1a3ce6272c60c0efb713c61ce12271e27
jnphilipp <jnphilipp@gmail.com>
2014-04-11 20:50:22 +0200

    Added model sources.models.Source.

--- infohub/settings.py
+++ infohub/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'south',
+	'sources',
 )
 
 MIDDLEWARE_CLASSES = (
--- sources/models.py
+++ sources/models.py
@@ -1,3 +1,30 @@
+from datetime import datetime
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.template.defaultfilters import slugify
+from south.modelsinspector import add_introspection_rules
+add_introspection_rules([], ["^sources\.models\.TextFieldSingleLine"])
 
-# Create your models here.
+class TextFieldSingleLine(models.TextField):
+	pass
+
+class Source(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	slug = models.SlugField(max_length=4096, unique=True)
+	url = TextFieldSingleLine(unique=True)
+	title = TextFieldSingleLine(blank=True, null=True)
+	update_time = models.DateTimeField(default=datetime.now)
+	alive = models.BooleanField(default=True)
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.slug = slugify(self.url)
+		super(Source, self).save(*args, **kwargs)
+
+	def __str__(self):
+		return self.title if self.title else self.url
+
+	class Meta:
+		abstract = True
