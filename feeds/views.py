4328888fdb0070c690ef0f69af7cb85d641ac679
jnphilipp <jnphilipp@gmail.com>
2014-04-23 00:04:50 +0200

    Updated views templates and style.

--- feeds/views.py
+++ feeds/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from feeds.models import Feed, Document, Report
 
 def feeds(request):
-	feeds = Feed.objects.order_by('alive', 'title')
+	feeds = Feed.objects.order_by('alive', '-updated_at')
 	return render(request, 'feeds/feeds.html', locals())
 
 def feed(request, slug):
