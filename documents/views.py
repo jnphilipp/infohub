8a33adf6a3aa729f83b559b1b45d49e6c8b84e4b
jnphilipp <jnphilipp@gmail.com>
2014-04-24 23:03:55 +0200

    Added login decorators.

--- documents/views.py
+++ documents/views.py
@@ -1,10 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 from documents.models import Document
 
+@login_required(login_url='/admin/')
 def documents(request):
 	documents = Document.objects.order_by('-created_at')
 	return render(request, 'documents/documents.html', locals())
 
+@login_required(login_url='/admin/')
 def document(request, slug):
 	document = get_object_or_404(Document, slug=slug)
 	return render(request, 'documents/document.html', locals())
--- feeds/views.py
+++ feeds/views.py
@@ -1,24 +1,30 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 from documents.models import Document
 from feeds.models import Feed, Report
 from datetime import datetime, date, time
 
+@login_required(login_url='/admin/')
 def feeds(request):
 	feeds = Feed.objects.order_by('alive', '-updated_at')
 	return render(request, 'feeds/feeds.html', locals())
 
+@login_required(login_url='/admin/')
 def feed(request, slug):
 	feed = get_object_or_404(Feed, slug=slug)
 	return render(request, 'feeds/feed.html', locals())
 
+@login_required(login_url='/admin/')
 def reports(request):
 	reports = Report.objects.order_by('-created_at')
 	return render(request, 'feeds/reports.html', locals())
 
+@login_required(login_url='/admin/')
 def report(request, slug):
 	report = get_object_or_404(Report, slug=slug)
 	return render(request, 'feeds/report.html', locals())
 
+@login_required(login_url='/admin/')
 def statistics(request):
 	today_min = datetime.combine(date.today(), time.min)
 	today_max = datetime.combine(date.today(), time.max)
